b9d4a3934c85445696c0d3c068aa40a1a93a294b
sidpd51 <sidpd510@gmail.com>
2025-08-18 09:27:23 +0530

    feat: stack class implementation in ts

---
+++ Stack/class.ts
@@ -0,0 +1,38 @@
+export default class Stack<T> {
+  private items: T[];
+
+  constructor() {
+    this.items = [];
+  }
+
+  push(element: T): void {
+    this.items.push(element);
+  }
+
+  pop(): T | undefined {
+    if (this.isEmpty()) {
+      throw new Error("Underflow");
+    }
+    return this.items.pop();
+  }
+
+  isEmpty(): boolean {
+    return this.items.length === 0;
+  }
+
+  peek(): T | undefined {
+    return this.items[this.items.length - 1];
+  }
+
+  size(): number {
+    return this.items.length;
+  }
+
+  clear(): void {
+    this.items = [];
+  }
+
+  print(): void {
+    console.log(this.items.toString());
+  }
+}
